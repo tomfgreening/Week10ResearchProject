c478a7dfe3b36c016fb7b13234a9adba328d27b9
Thomas Greening <tomfgreening@gmail.com>
2025-01-14 15:59:59 +0000

    Tom end of tuesday

--- server/server.js
+++ server/server.js
@@ -28,30 +28,11 @@ app.get("/moodthing", async (req, res) => {
   await res.json(query.rows);
 });
 
-// The code below needs to be added into the client when the form is ready to be pulled
-
-// // const messageForm = document.querySelector("#messageForm");
-
-// function handleSubmitMessageForm(event) {
-//   event.preventDefault();
-//   const formData = new FormData(messageForm);
-//   const formValues = Object.fromEntries(formData);
-//   fetch("http://localhost:8080/guestbookentry", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ formValues }),
-//   });
-//   console.log(formValues);
-// }
-// messageForm.addEventListener("submit", handleSubmitMessageForm);
-
-// app.post("/guestbookentry", async (req, res) => {
-//   const data = req.body.formValues;
-//   const query = await db.query(
-//     `INSERT INTO juicyjakesguestbook (col2, col3, col4, col5) VALUES ($1, $2, $3, $4)`,
-//     [data.input1, data.input2, data.input3, data.input4]
-//   );
-//   await res.json(query.rows);
-// });
+app.post("/moodTrackerEntry", async (req, res) => {
+  const data = req.body.formValues;
+  const query = await db.query(
+    `INSERT INTO moods (date, mood, comment) VALUES ($1, $2, $3)`,
+    [data.date, data.mood, data.comment]
+  );
+  await res.json(query.rows);
+});
